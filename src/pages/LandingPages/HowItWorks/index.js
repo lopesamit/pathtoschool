03583c650b3135c03cbebd1e70c4520272e398e9
Amit Lopes <lopesamit@gmail.com>
2022-08-29 21:37:44 -0400

    initial commit, some of path to school pages adopted

---
+++ src/pages/LandingPages/HowItWorks/index.js
@@ -0,0 +1,89 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+import MKTypography from "components/MKTypography";
+
+// Material Kit 2 React examples
+import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+// import DefaultFooter from "examples/Footers/DefaultFooter";
+
+// Routes
+import routes from "routes";
+import footerRoutes from "footer.routes";
+
+// Images
+import bgImage from "assets/images/bg-about-us.jpg";
+
+import CenteredFooter from "../../../examples/Footers/CenteredFooter";
+
+function HowItWorks() {
+  return (
+    <>
+      <DefaultNavbar routes={routes} sticky />
+      <MKBox
+        minHeight="75vh"
+        width="100%"
+        sx={{
+          backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>
+            `${linearGradient(
+              rgba(gradients.dark.main, 0.6),
+              rgba(gradients.dark.state, 0.6)
+            )}, url(${bgImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <Container>
+          <Grid
+            container
+            item
+            xs={12}
+            lg={8}
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+            sx={{ mx: "auto", textAlign: "center" }}
+          >
+            <MKTypography
+              variant="h1"
+              color="white"
+              sx={({ breakpoints, typography: { size } }) => ({
+                [breakpoints.down("md")]: {
+                  fontSize: size["3xl"],
+                },
+              })}
+            >
+              HowItWorks
+            </MKTypography>
+          </Grid>
+        </Container>
+      </MKBox>
+
+      <MKBox pt={6} px={1} mt={6}>
+        <CenteredFooter content={footerRoutes} />
+      </MKBox>
+    </>
+  );
+}
+
+export default HowItWorks;
